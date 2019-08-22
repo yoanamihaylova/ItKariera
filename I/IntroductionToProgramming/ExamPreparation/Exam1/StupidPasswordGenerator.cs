384ef4ed424372dcbc005ac1c2a2023fce53c295
yoanamihaylova <47273798+yoanamihaylova@users.noreply.github.com>
2019-08-22 21:51:33 +0300

    Add files via upload

---
+++ I/IntroductionToProgramming/ExamPreparation/Exam1/StupidPasswordGenerator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StupidPasswordGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, l;
+            n = int.Parse(Console.ReadLine());
+            l = int.Parse(Console.ReadLine());
+            for(int i=1;i<=n;i++)
+            {
+                for(int j=1;j<=n;j++)
+                {
+                    for(char k='a';k<'a'+l; k++)
+                    {
+                        for(char m='a';m<'a'+l;m++)
+                        {
+                            for (int t = 1; t <= n; t++)
+                            {
+                                if (t > i && t > j) Console.Write("{0}{1}{2}{3}{4} ",i,j,k,m,t);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ I/IntroductionToProgramming/ExamPreparation/Exam1/TrainingLab.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrainingLab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double w, h;
+            h = double.Parse(Console.ReadLine());
+            w = double.Parse(Console.ReadLine());
+            w = w * 100;
+            h = h * 100;
+            w = w - 100;
+            Console.WriteLine((int)(w/70)*(int)(h/120)-3);
+        }
+    }
+}
