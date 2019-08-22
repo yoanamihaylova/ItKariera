7bcbb1fccbbe8345618abe651edd073195407635
yoanamihaylova <47273798+yoanamihaylova@users.noreply.github.com>
2019-08-22 21:01:45 +0300

    Add files via upload

---
+++ I/IntroductionToProgramming/Intruduction/DrawSquare.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrawSquare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write('*');
+            }
+            Console.WriteLine();
+            for (int i = 1; i < n - 1; i++)
+            {
+                Console.Write('*');
+                for (int j = 0; j < n - 2; j++)
+                {
+                    Console.Write(" ");
+                }
+                Console.Write('*');
+                Console.WriteLine();
+            }
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write('*');
+            }
+
+        }
+    }
+}
---
+++ I/IntroductionToProgramming/Intruduction/RectangleArea.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RectangleArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal a;
+            a = decimal.Parse(Console.ReadLine());
+            var b = decimal.Parse(Console.ReadLine());
+            Console.WriteLine(a * b);
+
+        }
+    }
+}
