c19a5fca6ad7e680f55b0ab185e73fe80f491c46
yoanamihaylova <47273798+yoanamihaylova@users.noreply.github.com>
2019-08-24 21:22:53 +0300

    Add files via upload

---
+++ I/IntroductionToAlgorithmsAndDataStructures/ExamPreparation/Excersises/EightQueens/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+namespace EightQueens
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EightQueens.PutQueens(0);
+        }
+    }
+}
