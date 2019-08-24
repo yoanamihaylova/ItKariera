cf98ec1df2cddcbeb03da934e8e452e45f8f9f71
yoanamihaylova <47273798+yoanamihaylova@users.noreply.github.com>
2019-08-24 19:48:47 +0300

    Add files via upload

---
+++ I/Programing/DictionariesAndHashTables/LINQ (1).cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test4
+{
+    class Test4
+    {
+        static void Main(string[] args)
+        {
+            var arr = Console.ReadLine().Split().Select(int.Parse).ToList();
+            int n = arr.Count;
+            Console.WriteLine(n);
+            int evenCount = arr.Count(x => x % 2 == 0);
+            Console.WriteLine(evenCount);
+            int[] EvenNumbers = arr.Where(x => x % 2 == 0).ToArray();
+            Console.WriteLine(string.Join(" ", EvenNumbers));
+            arr.Reverse();
+            Console.WriteLine(string.Join(" ", arr));
+        }
+    }
+}
---
+++ I/Programing/DictionariesAndHashTables/LINQ (2).cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test3
+{
+    class Test3
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int n = arr.Length;
+            Console.WriteLine(n);
+            int evenCount = arr.Count(x => x % 2 == 0);
+            Console.WriteLine(evenCount);
+            int[] EvenNumbers = arr.Where(x => x % 2 == 0).ToArray();
+            Console.WriteLine(string.Join(" ",EvenNumbers));
+        }
+    }
+}
