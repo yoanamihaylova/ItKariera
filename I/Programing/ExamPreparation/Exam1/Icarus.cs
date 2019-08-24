eb8ca4daed4b5cf1ef9d1576209e9633ed3d95e9
yoanamihaylova <47273798+yoanamihaylova@users.noreply.github.com>
2019-08-24 19:51:30 +0300

    Add files via upload

---
+++ I/Programing/ExamPreparation/Exam1/Icarus.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Icarus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int index = int.Parse(Console.ReadLine());
+            string [] dic = Console.ReadLine().Split(' ').ToArray();
+            string dir = dic[0];
+            
+            }
+            }
+    }
+}
---
+++ I/Programing/ExamPreparation/Exam1/Resurrection.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Resurrection
+{
+    class Resurrection
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            decimal[] years = new decimal[N];
+            int lbody, wing;
+            decimal wbody;
+            for (int i = 0; i < N; i++)
+            {
+                lbody = int.Parse(Console.ReadLine());
+                wbody = decimal.Parse(Console.ReadLine());
+                wing = int.Parse(Console.ReadLine());
+                years[i] = (decimal)((decimal)lbody * lbody) * (wing * 2 + wbody);
+            }
+            for (int i = 0; i < N; i++) Console.WriteLine(years[i]);
+        }
+    }
+}
