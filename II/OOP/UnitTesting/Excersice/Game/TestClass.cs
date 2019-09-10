96cd5c4bae0e88d29709fe0de30bb30ffb36f38a
yoanamihaylova <47273798+yoanamihaylova@users.noreply.github.com>
2019-09-10 17:39:13 +0300

    Add files via upload

---
+++ II/OOP/UnitTesting/Excersice/Game/TestClass.cs
@@ -0,0 +1,41 @@
+﻿// NUnit 3 tests
+// See documentation : https://github.com/nunit/docs/wiki/NUnit-Documentation
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using NUnit.Framework;
+
+namespace AxeTests
+{
+    [TestFixture]
+    public class TestClass
+    {
+        [Test]
+        public void TestMethod()
+        {
+            // TODO: Add your test code here
+            var answer = 42;
+            Assert.That(answer, Is.EqualTo(42), "Some useful error message");
+        }
+        [Test]
+        public void AxeLosesDurabilityAfterAttack()
+        {
+            Axe axe = new Axe(10, 10);
+            Dummy dummy = new Dummy(10, 10);
+            axe.Attack(dummy);
+            Assert.AreEqual(9, axe.DurabilityPoints, "Axe Durability doesn't change after attack");
+        }
+        [Test]
+        public void BrokenAxeCanAttack()
+        {
+            Axe axe = new Axe(2, 2);
+            Dummy dummy = new Dummy(20, 20);
+            //Act
+            axe.Attack(dummy);
+            axe.Attack(dummy);
+            //Assert
+            var ex = Assert.Throws<InvalidOperationException>(() => axe.Attack(dummy));
+            Assert.That(ex.Message, Is.EqualTo("Axe is broken."));
+        }
+    }
+}
