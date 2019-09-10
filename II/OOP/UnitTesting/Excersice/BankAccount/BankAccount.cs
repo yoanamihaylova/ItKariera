14cde0173761da789dac04b007299d0d8b1ddd21
yoanamihaylova <47273798+yoanamihaylova@users.noreply.github.com>
2019-09-10 16:44:47 +0300

    Add files via upload

---
+++ II/OOP/UnitTesting/Excersice/BankAccount/BankAccount.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccounts
+{
+    public class BankAccount
+    {
+        private decimal amount;
+
+        public decimal Amount
+        {
+            get { return amount; }
+            set { amount = value; }
+        }
+
+        public BankAccount(decimal sum = 0)
+        {
+            Amount = sum;
+        }
+        public void Deposit (decimal sum)
+        {
+            Amount += sum;
+        }
+        public void Withdraw (decimal sum)
+        {
+            if (sum > Amount) throw new InvalidOperationException("Insufficient availability!");
+            Amount -= sum;
+        }
+
+    }
+}
