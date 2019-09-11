cedbff44afdaf11e13008c910abfae0f6f6fcf39
yoanamihaylova <47273798+yoanamihaylova@users.noreply.github.com>
2019-09-11 15:53:13 +0300

    Add files via upload

---
+++ II/OOP/UnitTesting/Excersice/InterfacesGame/Interfaces/ITarget.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skeleton.Interfaces
+{
+    public interface ITarget
+    {
+        int Health
+        {
+            get;
+        }
+
+        void TakeAttack(int attackPoints);
+
+        int GiveExperience();
+
+        bool IsDead();
+    }
+}
---
+++ II/OOP/UnitTesting/Excersice/InterfacesGame/Interfaces/IWeapon.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skeleton.Interfaces
+{
+    public interface IWeapon
+    {
+
+        int AttackPoints
+        {
+            get;
+        }
+
+        int DurabilityPoints
+        {
+            get;
+        }
+        void Attack(ITarget target);
+    }
+}
