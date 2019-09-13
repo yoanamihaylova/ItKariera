6ac8e05b7d5cc0229c384627ac2f3b1496fc9215
yoanamihaylova <47273798+yoanamihaylova@users.noreply.github.com>
2019-09-13 16:05:28 +0300

    Add files via upload

---
+++ II/OOP/DefiningClasses/Excersice/MemoryManagment/Peoples.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemoryManagment
+{
+    public class Person
+    {
+        public string name { get; set; }
+        public int age { get; set; }
+        public Person(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+    }
+    public class Peoples : IDisposable
+    {
+        public List<Person> peoples;
+        public void Add(Person person)
+        {
+            peoples.Add(person);
+        }
+        public void Dispose()
+        {
+            Console.WriteLine("Dispose...");
+            this.peoples = null;
+        }
+        public Peoples()
+        {
+            peoples = new List<Person>(); //heap
+            Console.WriteLine("Using constructor...");
+        }
+        ~Peoples()
+        {
+            Console.WriteLine("Using destructor...");
+        }
+    }
+}
---
+++ II/OOP/DefiningClasses/Excersice/MemoryManagment/StartUp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemoryManagment
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Peoples list = new Peoples();
+            list.Add(new Person("Anna", 17));
+            list.Add(new Person("Didi", 16));
+            list.Add(new Person("Bobi", 19));
+            Console.WriteLine("Peoples Count = {0}",list.peoples.Count);
+
+            list.Dispose();
+        }
+    }
+}
