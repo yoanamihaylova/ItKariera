ec3db978c5949d232c491200f438cd50ee72acd8
yoanamihaylova <47273798+yoanamihaylova@users.noreply.github.com>
2019-09-13 18:10:10 +0300

    Add files via upload

---
+++ II/OOP/DefiningClasses/Excersice/MemoryManagment/BoxTask/Box.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoxTask
+{
+    class Box<T>
+    {
+        private List<T> data;
+        public List<T> Data
+        {
+            get { return data; }
+            set { data = value; }
+        }
+        public Box()
+        {
+            Data = new List<T>();
+        }
+        public void Add(T element)
+        {
+            data.Add(element);
+        }
+        public T Remove()
+        {
+            int index = data.Count - 1;
+            T element = data[data.Count - 1];
+
+            data.RemoveAt(index);
+            return element;
+        }
+    }
+}
