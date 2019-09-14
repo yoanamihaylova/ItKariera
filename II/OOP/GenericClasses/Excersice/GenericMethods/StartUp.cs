3a98b4f270ca90e09937a1eb665a3f8d400246a0
yoanamihaylova <47273798+yoanamihaylova@users.noreply.github.com>
2019-09-14 14:44:36 +0300

    Add files via upload

---
+++ II/OOP/GenericClasses/Excersice/GenericMethods/StartUp.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericMethods
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<string> boxes = new List<string>();
+            
+            for (int i = 0; i < n ; i++)
+            {
+                string line = Console.ReadLine();
+                boxes.Add(line);
+            }
+            int []positions = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            StringSwapping.Swap(boxes, positions[0], positions[1]);
+            foreach (var item in boxes)
+            {
+                Console.WriteLine(item.GetType() + " : " + item);
+            }
+        }
+    }
+}
---
+++ II/OOP/GenericClasses/Excersice/GenericMethods/StringSwapping.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericMethods
+{
+    public static class StringSwapping
+    {
+        public static void Swap<T>(List<T> list, int index1, int index2 )
+        {
+            T temp = list[index1];
+            list[index1] = list[index2];
+            list[index2] = temp;
+        }
+    }
+}
