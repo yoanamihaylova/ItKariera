58bc40b30b2e6210a9936ac0aabfdb335df099f7
yoanamihaylova <47273798+yoanamihaylova@users.noreply.github.com>
2019-09-14 16:08:40 +0300

    Add files via upload

---
+++ II/OOP/GenericClasses/Excersice/CustomList/CustomList.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomList
+{
+    public class CustomList<T> where T : IComparable<T>
+    {
+        private List<T> list ;
+
+        public List<T> List
+        {
+            get { return list; }
+            set { list = value; }
+        }
+
+        public CustomList()
+        {
+            this.List = new List<T>();
+        }
+        public void Add(T element)
+        {
+            List.Add(element);
+        }
+	    public T Remove(int index)
+        {
+            T temp = list[index];
+            list.RemoveAt(index);
+            return temp;            
+        }
+	    public  bool Contains(T element)
+        {
+            foreach (var item in list)
+            {
+                if (item.Equals(element)) return true;
+            }
+            return false;
+        }
+	    public void Swap(int index1, int index2)
+        {
+            T temp = list[index1];
+            list[index1] = list[index2];
+            list[index2] = temp;
+        }
+	    public int CountGreaterThan(T element)
+        {
+            int br = 0;
+            foreach (var item in list)
+            {
+                if (item.CompareTo(element) > 0) br++;
+            }
+            return br;
+        }
+        public T Max()
+        {
+            return list.Max();
+        }
+	    public T Min()
+        {
+            return list.Min();
+        }
+        public override string ToString()
+        {
+            string temp = "";
+            foreach (var item in list)
+            {
+                temp += $"{item}";
+            }
+            return temp;
+        }
+    }
+}
---
+++ II/OOP/GenericClasses/Excersice/CustomList/StartUp.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomList
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string []line = Console.ReadLine().Split(' ').ToArray();
+            CustomList<string> list = new CustomList<string>();
+            while (line[0] != "END")
+            {
+                
+                if(line[0] == "Add")
+                {
+                    list.Add(line[1]);
+                }
+                else if(line[0] == "Remove")
+                {
+                    Console.WriteLine(list.Remove(int.Parse(line[1]))); 
+                }
+                else if(line[0] == "Conatins")
+                {
+                    Console.WriteLine(list.Contains(line[1])); 
+                }
+                else if(line[0] == "Swap")
+                {
+                    list.Swap((int.Parse(line[1])), (int.Parse(line[2])));
+                }
+                else if(line[0] == "Greater")
+                {
+                    Console.WriteLine( list.CountGreaterThan(line[1]) ); 
+                }
+                else if(line[0] == "Max")
+                {
+                    Console.WriteLine( list.Max());
+                }
+                else if(line[0] == "Min")
+                {
+                    Console.WriteLine( list.Min() ); 
+                }
+                else if(line[0] == "Print")
+                {
+                    list.ToString();
+                }
+                line = Console.ReadLine().Split(' ').ToArray();
+            }
+        }
+    }
+}
