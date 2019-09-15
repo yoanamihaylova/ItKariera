721e3f0aaf5d7fdb0b70d3e3bfcb57684d1e814d
yoanamihaylova <47273798+yoanamihaylova@users.noreply.github.com>
2019-09-15 14:45:17 +0300

    Add files via upload

---
+++ II/OOP/Inheritance/Excercise/StackOfStrings/Stack.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackOfStrings
+{
+    class Stack
+    {
+        private List<String> list = new List<string>();
+        public void Push(string element)
+        {
+            list.Add(element);
+        }
+        public string Pop()
+        {
+            string last = list[list.Count - 1];
+            list.RemoveAt(list.Count - 1);
+            return last;
+        }
+        public string Peek()
+        {
+            string last = list[list.Count - 1];
+            return last;
+        }
+        public bool IsEmpty()
+        {
+            if (list.Count > 0) return false;
+            else return true;
+        }
+        public override string ToString()
+        {
+            var result = "";
+            foreach (var item in list)
+            {
+                result += item + " ";
+            }
+            return result.TrimEnd();
+        }
+    }
+}
---
+++ II/OOP/Inheritance/Excercise/StackOfStrings/StartUp.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackOfStrings
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Stack stack = new Stack();
+            string []line = Console.ReadLine().Split(' ').ToArray();
+            foreach (var item in line)
+            {
+                stack.Push(item);
+            }
+            Console.WriteLine(stack);
+            while(stack.IsEmpty()!=true)
+            {
+                stack.Pop();
+            }
+            
+            Console.WriteLine(stack.IsEmpty());
+        }
+    }
+}
