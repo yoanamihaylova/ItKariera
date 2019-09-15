42f6132f89ab453a6d842ef46965877ba39af3ef
yoanamihaylova <47273798+yoanamihaylova@users.noreply.github.com>
2019-09-15 14:43:18 +0300

    Add files via upload

---
+++ II/OOP/Inheritance/Excercise/CustomArrayList/CustomArrayList.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomArrayList
+{
+    class CustomArrayList : ArrayList
+    {
+        private Random rnd = new Random();
+        public object RandomElement()
+        {
+            int index = rnd.Next(0, base.Count - 1);
+            object element = base[index];
+            base.Remove(element);
+            return element;
+
+        }
+    }
+}
---
+++ II/OOP/Inheritance/Excercise/CustomArrayList/StartUp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomArrayList
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            CustomArrayList customArrayList = new CustomArrayList();
+            var input = Console.ReadLine().Split(' ');
+            foreach (var item in input)
+            {
+                customArrayList.Add(item);
+            }
+            Console.WriteLine(customArrayList.Count);
+            Console.WriteLine(customArrayList.RandomElement());
+            Console.WriteLine(customArrayList.Count);
+        }
+    }
+}
