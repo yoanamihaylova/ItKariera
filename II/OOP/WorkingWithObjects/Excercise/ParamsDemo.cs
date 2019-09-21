c277ae3ee85888bc08236e645b8c6c59fee0f843
yoanamihaylova <47273798+yoanamihaylova@users.noreply.github.com>
2019-09-21 13:40:22 +0300

    Add files via upload

---
+++ II/OOP/WorkingWithObjects/Excercise/ParamsDemo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParamsDemo
+{
+    class ParamsDemo
+    {
+        static void Main(string[] args)
+        {
+            PrintNames("Pesho", "Stamat", "Jivko", "Stavri");
+            
+        }
+
+        public static void PrintNames(params string[] names)
+        {
+            foreach (var name in names)
+            {
+                Console.WriteLine(name);
+            }
+        }
+
+    }
+}
