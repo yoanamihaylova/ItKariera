22467574d79d54d855d6f338580704c55c562b45
yoanamihaylova <47273798+yoanamihaylova@users.noreply.github.com>
2019-09-21 17:42:34 +0300

    Add files via upload

---
+++ II/OOP/WorkingWithObjects/Excercise/ListyIterator/ListyIterator.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListyIterator
+{
+    public class ListyIterator <T> : IEnumerable<T>
+    {
+        private List<T> listy;
+        private int index = 0;
+        public ListyIterator(params T []listy)
+        {
+            this.Create(listy);
+        }
+
+        public void Create(params T[] listy)
+        {
+            this.listy = new List<T>(listy);
+        }
+        public bool Move()
+        {
+            if (!HasNext()) return false;
+            else
+            {
+                this.index++;
+                return true;
+            }
+        }
+        public bool HasNext()
+        {
+            if (this.index == this.listy.Count - 1) return false;
+            else return true;
+        }
+        public void Print()
+        {
+            if (this.listy.Count == 0) throw new Exception("Invalid Operation");
+            else Console.WriteLine(this.listy[index]);
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            foreach (var item in listy)
+            {
+                yield return item;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+       /* public void PrintAll()
+        {
+            foreach (var item in listy)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+        }*/
+    }
+}
---
+++ II/OOP/WorkingWithObjects/Excercise/ListyIterator/StartUp.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListyIterator
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            ListyIterator<string> list = new ListyIterator<string>();
+            var line = Console.ReadLine().Split(' ').ToArray();
+            while(line[0] != "END")
+            {
+                if(line[0] == "Create")
+                {
+                    list.Create(line.Skip(1).ToArray());
+                }
+                if(line[0] == "HasNext")
+                {
+                    Console.WriteLine(list.HasNext());
+                }
+                if(line[0] == "Print")
+                {
+                    list.Print();
+                }
+                if(line[0] == "PrintAll")
+                {
+                    foreach (var item in list)
+                    {
+                        Console.Write(item + " ");
+                    }
+                    Console.WriteLine();
+                }
+                if(line[0] == "Move")
+                {
+                    Console.WriteLine(list.Move());
+                }
+                line = Console.ReadLine().Split(' ').ToArray();
+            }
+        }
+    }
+}
