6ad71c3ef2f1e7ac9921e037e1ef3aa3421adf9e
yoanamihaylova <47273798+yoanamihaylova@users.noreply.github.com>
2019-10-13 10:08:13 +0300

    Add files via upload

--- II/OOP/FunctionalProgramming/FunctionsTesting.cs
+++ II/OOP/FunctionalProgramming/FunctionsTesting.cs
@@ -14,6 +14,12 @@ namespace FunctionsTesting
 
             Action<string> print = meseage => Console.WriteLine(meseage);
             print("Hello,world!");
+
+            int a = 5;
+            int b = Operation(a, number => number * 5);
+            int c = Operation(a, number => number - 3);
+            int d = Operation(b, number => number % 2);
+
         }
         private static int Operation(int number, Func<int, int> operation)
         {
