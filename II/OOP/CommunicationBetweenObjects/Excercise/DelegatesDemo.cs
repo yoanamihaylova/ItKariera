684cd07c6c607302a563bc80514ea3cd61261cd3
yoanamihaylova <47273798+yoanamihaylova@users.noreply.github.com>
2019-10-13 13:56:19 +0300

    Add files via upload

---
+++ II/OOP/CommunicationBetweenObjects/Excercise/DelegatesDemo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Demo2
+{
+    class DelegatesDemo
+    {
+        public delegate int myDelegate();
+        public delegate void myDelegates();
+        
+        static void Main(string[] args)
+        {
+            /* myDelegate myDeleg = RandomNumber;
+             Console.WriteLine(myDeleg());*/
+            myDelegates d = f1;
+            d += f2;
+            d += f3;
+            d();
+
+            d -= f2;
+            d();
+
+
+        }
+        public static int RandomNumber()
+        {
+            Random rnd = new Random();
+            return rnd.Next(100);
+        }
+        public static void f1()
+        { Console.WriteLine("Execute first method!"); }
+        public static void f2()
+        { Console.WriteLine("Execute second method"); }
+        public static void f3()
+        { Console.WriteLine("Execute third method"); }
+    }
+}
---
+++ II/OOP/CommunicationBetweenObjects/Excercise/EventsDemo.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Events
+{
+    class Events
+    {
+        static void Main(string[] args)
+        {
+            Student student1 = new Student();
+            student1.Name = "Ivan";
+            Student student2 = new Student();
+            student2.Name = "Ani";
+            Student student3 = new Student();
+            student3.Name = "Didi";
+            Teacher teacher = new Teacher();
+            teacher.Name = "Ivanov";
+
+            student1.Enroll(teacher);
+            student2.Enroll(teacher);
+            student3.Enroll(teacher);
+            teacher.StartCourse();
+        }
+    }
+    class Student
+    {
+        public string Name { get; set; }
+
+        public void Enroll(Teacher teacher)
+        {
+            teacher.OnStartCourse += EnterInClassroom;
+        }
+        private void EnterInClassroom(object sender, EventArgs args)
+        {
+            Console.WriteLine($"{this.Name} enter in classroom!");
+        }
+    }
+    class Teacher
+    {
+        public string Name { get; set; }
+       // public delegate void SubscribeToClass();
+        public event EventHandler OnStartCourse;
+
+        public void StartCourse()
+        {
+            OnStartCourse(this, EventArgs.Empty);
+        }
+    }
+}
