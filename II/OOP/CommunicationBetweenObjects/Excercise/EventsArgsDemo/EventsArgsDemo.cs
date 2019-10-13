7c6c086622e6154c6d9923267b1ddd78cb424676
yoanamihaylova <47273798+yoanamihaylova@users.noreply.github.com>
2019-10-13 13:59:39 +0300

    Add files via upload

---
+++ II/OOP/CommunicationBetweenObjects/Excercise/EventsArgsDemo/EventsArgsDemo.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+
+namespace EVentArgsDemo
+{
+    public delegate void myDelegate(object sender, MyEventArgs args);
+    public class Person
+    {
+        private string name;
+        public event myDelegate handler;
+
+        public string Name
+        {
+            
+            get { return name; }
+            set { 
+                MyEventArgs args = new MyEventArgs();
+                args.OldName = this.name;
+                name = value;
+                Notify(args);
+            }
+        }
+
+        private void Notify(MyEventArgs args)
+        {
+            handler(this, args);
+        }
+
+        public Person(string name)
+        {
+            this.name = name;
+        }
+    }
+    public class EventsArgsDemo
+    { 
+        static void Main(string[] args)
+        {
+            Person person = new Person("Pesho");
+            person.handler += (sender, argss) =>
+            {
+                Person senderObj = (Person)sender;
+                Console.WriteLine($"Change sender name from {argss.OldName} to {senderObj.Name}.");
+            };
+            person.Name = "Ivan";
+        }
+    }
+}
---
+++ II/OOP/CommunicationBetweenObjects/Excercise/EventsArgsDemo/MyEventArgs.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EVentArgsDemo
+{
+   public class MyEventArgs : EventArgs
+    {
+        public string OldName { get; set; }
+    }
+}
