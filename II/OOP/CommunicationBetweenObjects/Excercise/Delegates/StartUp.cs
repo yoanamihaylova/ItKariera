f17622e12b88a1099499448278c4d69c5418ec93
yoanamihaylova <47273798+yoanamihaylova@users.noreply.github.com>
2019-10-13 13:55:25 +0300

    Add files via upload

---
+++ II/OOP/CommunicationBetweenObjects/Excercise/Delegates/StartUp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Delegates
+{
+    class StartUp
+    {
+        public delegate void WorkPerformedHandler(int hours, WorkType workType);
+        static void Main(string[] args)
+        {
+            WorkPerformedHandler w = ManagerWorkPerformed;
+            w(2, WorkType.Admin);
+
+            WorkPerformedHandler w1 = ManagerWorkPerformed;
+            w += w1;
+            w(5, WorkType.User);
+        }
+        public static void ManagerWorkPerformed(int workHours, WorkType wType)
+        {
+            Console.WriteLine($"{wType} worked {workHours} hours.");
+        }
+    }
+}
---
+++ II/OOP/CommunicationBetweenObjects/Excercise/Delegates/WorkType.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Delegates
+{
+    public enum WorkType
+    {
+        SuperAdmin, Admin, User
+    }
+}
