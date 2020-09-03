2ead5dad605cea64dc0a883756d562a6aea1081a
yoanamihaylova <47273798+yoanamihaylova@users.noreply.github.com>
2020-09-03 14:00:35 +0300

    Add files via upload

---
+++ III/AlgorithmsAndDataStructures/Exercises/Recursion/TowerOfHanoi.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace TowerOfHanoi
+{
+    class TowerOfHanoi
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Hanoi(n, 1, 2, 3);
+        }
+
+        private static void Hanoi(int n, int source, int help, int target)
+        {
+            if (n == 0) return;
+            Hanoi(n - 1, source, target, help);
+            Console.WriteLine(source + " --> " + target);
+            Hanoi(n - 1, help, source, target);
+        }
+    }
+}
