d44b7c0c234c9d52d6c8051473ce95d3f8f82180
yoanamihaylova <47273798+yoanamihaylova@users.noreply.github.com>
2020-09-03 11:09:46 +0300

    Add files via upload

---
+++ III/AlgorithmsAndDataStructures/Exercises/Recursion/Fibonacci.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    class Fibonacci
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine(Fib(n));
+        }
+        static int Fib (int n)
+        {
+            if (n == 1 || n == 2) return 1;
+            return Fib(n - 1) + Fib(n-2);
+        }
+    }
+}
---
+++ III/AlgorithmsAndDataStructures/Exercises/Recursion/GCD.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace GCD
+{
+    class GCD
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine(GCD_Subtraction(a,b));
+            Console.WriteLine(GCD_Division(a,b));
+        }
+        static int GCD_Subtraction(int a, int b)
+        {
+            
+            if (a == b) return a;
+            if (a > b) return GCD_Subtraction(a - b, b);
+            else return GCD_Subtraction(a, b - a);
+        }
+        static int GCD_Division(int a, int b)
+        {
+            if (b == 0) return a;
+            return GCD_Division(b, a % b);
+        }
+    }
+}
