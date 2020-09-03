5b368489b1a2bf7fe3a59b02846b6157cd31cb93
yoanamihaylova <47273798+yoanamihaylova@users.noreply.github.com>
2020-09-03 13:30:25 +0300

    За оправяне!

---
+++ III/AlgorithmsAndDataStructures/Exercises/Recursion/ChessKnight.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Horses
+{
+    class ChessKnight
+    {
+        static int n;
+        static int count;
+        static int current;
+        static int[,] board = new int[n, n];
+        static void Main(string[] args)
+        {
+            n = int.Parse(Console.ReadLine());
+            Knight(0, 0);
+        } 
+        private static void Knight(int row, int col)
+        {
+                        
+            if (current == n * n )
+            {
+                Print();
+                return;
+            }
+            if (row < 0 || row >= n || col < 0 || col >= n) return;
+            if (board[row, col] > 0) return;
+
+            current++;
+            board[row, col] = current;
+            Knight(row - 2, col + 1);
+            Knight(row - 1, col + 2);
+            Knight(row + 1, col + 2);
+            Knight(row + 2, col + 1);
+            Knight(row + 2, col - 1);
+            Knight(row + 1, col - 2);
+            Knight(row - 1, col - 2);
+            Knight(row - 2, col - 1);
+            current--;
+            board[row, col] = 0;
+        }
+
+        private static void Print()
+        {
+            count++;
+            Console.WriteLine($"Solution: {count}");
+            for (int row = 0; row < n; row++)
+            {
+                for (int col = 0; col < n; col++)
+                {
+                    Console.Write(board[row,col] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+   
+}
