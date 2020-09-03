f1f26bc5ea88b06f123afeeb0bcbdd0aebce400e
yoanamihaylova <47273798+yoanamihaylova@users.noreply.github.com>
2020-09-03 11:08:50 +0300

    Не работещ!

---
+++ III/AlgorithmsAndDataStructures/Exercises/Recursion/LCM.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace LCM
+{
+    class LCM
+    {
+        static void Main(string[] args)
+        {
+           //Не работи!
+            
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine(a*b/GCD_Division(a,b));
+        }
+        static int GCD_Division(int a, int b)
+        {
+            if (b == 0) return a;
+            return GCD_Division(b, a % b);
+        }
+
+    }
+}
