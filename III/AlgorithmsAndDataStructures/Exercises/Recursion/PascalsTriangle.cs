5f2473ed5e92f4d25d8aa04f08c612fbbc2f88f1
yoanamihaylova <47273798+yoanamihaylova@users.noreply.github.com>
2020-09-04 13:47:56 +0300

    Add files via upload

---
+++ III/AlgorithmsAndDataStructures/Exercises/Recursion/PascalsTriangle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace PascalsTriangle
+{
+    class PascalsTriangle
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            //int k = int.Parse(Console.ReadLine());
+            for (int i = 0; i <= n; i++)
+            {
+                Console.Write(Binom(n,i) + " ");
+            }
+
+        }
+        private static int Binom (int n, int k)
+        {
+            if (n == k || k == 0) return 1;
+            return Binom(n - 1, k) + Binom(n - 1, k - 1);
+        }
+    }
+}
