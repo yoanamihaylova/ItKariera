8b333a814ecdd1752d966e260040e46f2baacb7f
yoanamihaylova <47273798+yoanamihaylova@users.noreply.github.com>
2020-09-04 13:24:50 +0300

    Add files via upload

---
+++ III/AlgorithmsAndDataStructures/Exercises/Recursion/CombVol2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp2
+{
+    class CombVol2
+    {
+        static int[] combs;
+        static int k;
+        static int n;
+        static void Main(string[] args)
+        {
+            n = int.Parse(Console.ReadLine());
+            k = int.Parse(Console.ReadLine());
+            combs = new int[n];
+            Gen(0, 0);
+        }
+
+        private static void Gen(int index, int start)
+        {
+            if (index == k) Console.WriteLine(string.Join(" ", combs.Take(k)));
+            for (int i = start; i < n; i++)
+            {
+                combs[index] = i+1;
+                Gen(index + 1, i + 1);
+                // var[index] = 0;
+            }
+        }
+    }
+}
---
+++ III/AlgorithmsAndDataStructures/Exercises/Recursion/VarVol2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp3
+{
+    class VarVol2
+    {
+        static bool[] used;
+        static int[] var;
+        static int k;
+        static int n;
+        static void Main(string[] args)
+        {
+            n = int.Parse(Console.ReadLine());
+            k = int.Parse(Console.ReadLine());
+            used = new bool[n];
+            var = new int[n];
+            Gen(0);
+        }
+
+        private static void Gen(int index)
+        {
+            if (index == k) Console.WriteLine(string.Join(" ", var.Take(k)));
+            else
+                for (int i = 0; i < n; i++)
+                {
+                    if (!used[i])
+                    {
+                        used[i] = true;
+                        var[index] = i+1;
+                        Gen(index + 1);
+                        used[i] = false;
+                    }
+                }
+        }
+    }
+}
