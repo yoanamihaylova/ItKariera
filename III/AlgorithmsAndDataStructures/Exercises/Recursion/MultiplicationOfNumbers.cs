99bd7f58c19e62c9f25a5c411142743ced8ebca7
yoanamihaylova <47273798+yoanamihaylova@users.noreply.github.com>
2020-09-05 11:41:14 +0300

    Add files via upload

---
+++ III/AlgorithmsAndDataStructures/Exercises/Recursion/MultiplicationOfNumbers.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace MultiplicationOfNumbers
+{
+    class MultiplicationOfNumbers
+    {
+        static int n;
+        static int[] numbers;
+
+        static void Main(string[] args)
+        {
+            n = int.Parse(Console.ReadLine());
+            numbers = new int[n];
+            Destruction(n, 0);
+        }
+        static void Destruction(int n, int pos)
+        {
+            if (n == 1)
+            {
+                Console.WriteLine(string.Join("+", numbers.Take(pos)));
+                return;
+            }
+            for (int i = n; i > 1; i--)
+            {
+                if (n % i == 0)
+                {
+                    numbers[pos] = i;
+                    if (pos == 0 || numbers[pos] <= numbers[pos - 1]) Destruction(n / i, pos + 1);
+                }
+            }
+        }
+    }
+}
