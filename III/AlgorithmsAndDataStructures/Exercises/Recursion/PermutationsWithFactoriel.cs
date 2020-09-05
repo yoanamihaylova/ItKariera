583389ca2ff00c2a80e6cae4420ad835c7312070
yoanamihaylova <47273798+yoanamihaylova@users.noreply.github.com>
2020-09-05 10:04:45 +0300

    Add files via upload

---
+++ III/AlgorithmsAndDataStructures/Exercises/Recursion/PermutationsWithFactoriel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Numerics;
+
+namespace PermutationsWithFactoriel
+{
+    class PermutationsWithFactoriel
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine( Fact(n));
+        }
+        static BigInteger Fact(int n)
+        {
+            if (n == 1) return 1;
+            return n * Fact(n - 1);
+        }
+    }
+}
