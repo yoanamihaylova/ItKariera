8372f64154cdeb29836c6e1f67781d3bcc58510e
yoanamihaylova <47273798+yoanamihaylova@users.noreply.github.com>
2020-09-27 14:27:32 +0300

    Add files via upload

---
+++ III/AlgorithmsAndDataStructures/Exercises/Trees/Problem2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Problem2
+{
+    class Program
+    {
+        static BinaryTree<int> binaryTree = new BinaryTree<int>();
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
