440c563659ddc11592f9d38ce5784e694d9bf43d
yoanamihaylova <47273798+yoanamihaylova@users.noreply.github.com>
2020-09-27 14:29:53 +0300

    Add files via upload

---
+++ III/AlgorithmsAndDataStructures/Exercises/Hash/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hash
+{
+    public class Person
+    {
+        public string firstName;
+        public string lastName;
+        public int age;
+        public Person(string firstName, string lastName, int age)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.age = age;
+        }
+        public override int GetHashCode()
+        {
+            int a = firstName.GetHashCode() + age;
+            int b = lastName.GetHashCode() + age;
+            return a + b;
+        }
+    }
+}
---
+++ III/AlgorithmsAndDataStructures/Exercises/Hash/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Hash
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Person person = new Person("Pesho", "Peshev", 12);
+            Console.WriteLine(person.GetHashCode());
+        }
+    }
+}
