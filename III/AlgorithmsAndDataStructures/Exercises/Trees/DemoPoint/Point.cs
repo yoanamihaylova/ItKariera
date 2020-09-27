c02308768cfcc0180384adb1c4e8b630536cb833
yoanamihaylova <47273798+yoanamihaylova@users.noreply.github.com>
2020-09-27 14:29:04 +0300

    Add files via upload

---
+++ III/AlgorithmsAndDataStructures/Exercises/Trees/DemoPoint/Point.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace DemoPoint
+{
+    public class Point : IComparable<Point>
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Point) || obj == null) return false;
+            Point point = (Point)obj;
+            return (X == point.X && Y == point.Y);
+        }
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+        public override int GetHashCode()
+        {
+            return (X << 16 | X >> 16) ^ Y;
+        }
+
+        public int CompareTo(Point other)
+        {
+            if(X != other.X)
+            {
+                return X.CompareTo(other.X);
+            }
+            else
+            {
+                return Y.CompareTo(other.Y);
+            }
+        }
+    }
+}
---
+++ III/AlgorithmsAndDataStructures/Exercises/Trees/DemoPoint/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DemoPoint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point p1 = new Point(3, 4);
+            Console.WriteLine(p1.GetHashCode());
+        }
+    }
+}
