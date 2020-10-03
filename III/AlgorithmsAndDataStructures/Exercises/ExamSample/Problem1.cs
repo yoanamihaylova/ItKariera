5893b0cfe1322ce1d619ed47528bf88df58b6539
yoanamihaylova <47273798+yoanamihaylova@users.noreply.github.com>
2020-10-03 10:19:49 +0300

    Add files via upload

---
+++ III/AlgorithmsAndDataStructures/Exercises/ExamSample/Problem1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Problem1
+{
+    class Problem1
+    {
+        static void Main(string[] args)
+        {
+            int workingDays = int.Parse(Console.ReadLine());
+            if (workingDays * 20 % 100 == 0) Console.WriteLine(workingDays*20/100);
+            else
+            {
+                int vauchers = 0;
+                vauchers += workingDays*20 / 100;
+                if (vauchers == 0) vauchers += workingDays * 20 / 20;
+                else
+                {
+                    workingDays -= 100 * vauchers / 20;
+                    vauchers += workingDays * 20 / 20;
+                }
+                Console.WriteLine(vauchers);
+            }
+        }
+    }
+}
---
+++ III/AlgorithmsAndDataStructures/Exercises/ExamSample/Problem2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace Problem2
+{
+    class Problem2
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int number1 = input[0];
+            int number2 = input[1];
+            Console.WriteLine(Multiply(number1, number2));
+        }
+        static int Multiply(int a, int b)
+        {            
+            if (a == 0 || b == 0) return 0;
+            else if ( b == 1) return a;
+            return a + Multiply(a, b - 1);
+        }
+    }
+}
